f8ff00b71aae505c5ed21a2601f14816923b4535
FSV.lab <fsaldana7@misena.edu.co>
2022-11-08 12:51:28 -0500

    Agregando proyecto1

---
+++ src/aplicacion1/Aplicacion1.java
@@ -0,0 +1,18 @@
+
+
+package aplicacion1;
+
+/**
+ *
+ * @author ferch
+ */
+public class Aplicacion1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
